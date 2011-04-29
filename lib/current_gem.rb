63f93db6682426505e94407fddf2bf962d5807aa
Jens Wille <jens.wille@uni-koeln.de>
2011-04-29 14:58:30 +0200

    Switched license to Affero GPL.

--- lib/current_gem.rb
+++ lib/current_gem.rb
@@ -3,25 +3,25 @@
 #                                                                             #
 # current_gem -- Maintain symlinks to current RubyGems                        #
 #                                                                             #
-# Copyright (C) 2009 University of Cologne,                                   #
-#                    Albertus-Magnus-Platz,                                   #
-#                    50923 Cologne, Germany                                   #
+# Copyright (C) 2009-2011 University of Cologne,                              #
+#                         Albertus-Magnus-Platz,                              #
+#                         50923 Cologne, Germany                              #
 #                                                                             #
 # Authors:                                                                    #
 #     Jens Wille <jens.wille@uni-koeln.de>                                    #
 #                                                                             #
 # current_gem is free software; you can redistribute it and/or modify it      #
-# under the terms of the GNU General Public License as published by the Free  #
-# Software Foundation; either version 3 of the License, or (at your option)   #
-# any later version.                                                          #
+# under the terms of the GNU Affero General Public License as published by    #
+# the Free Software Foundation; either version 3 of the License, or (at your  #
+# option) any later version.                                                  #
 #                                                                             #
 # current_gem is distributed in the hope that it will be useful, but WITHOUT  #
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       #
-# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for    #
-# more details.                                                               #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License #
+# for more details.                                                           #
 #                                                                             #
-# You should have received a copy of the GNU General Public License along     #
-# with current_gem. If not, see <http://www.gnu.org/licenses/>.               #
+# You should have received a copy of the GNU Affero General Public License    #
+# along with current_gem. If not, see <http://www.gnu.org/licenses/>.         #
 #                                                                             #
 ###############################################################################
 #++
